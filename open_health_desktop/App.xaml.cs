4baeb4867d8a693cfe8feea2757067ab20ef4b06
Dombi Balázs Sándor <dombi.balazs.sandor@gmail.com>
2025-11-04 09:34:22 +0100

    changed avalonia to wpf

---
+++ open_health_desktop/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace open_health_desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
